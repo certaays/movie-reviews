74d0be62dc8a916fc9de02b300f1ad5779517de5
certaays <certaays@gmail.com>
2024-10-17 00:19:50 +0700

    back to origin

--- database/database.go
+++ database/database.go
@@ -12,6 +12,7 @@ var DB *gorm.DB
 func DatabaseInit() {
 	var err error
 	// const MYSQL = "root:@tcp(127.0.0.1:3306)/learn-go?charset=utf8mb4&parseTime=True&loc=Local"
+
 	const POSTGRESQL = "host=ep-shiny-paper-a1zyqayd-pooler.ap-southeast-1.aws.neon.tech user=default password=5LkQIZpBY7Tf dbname=verceldb sslmode=require TimeZone=Asia/Shanghai"
 
 	dsn := POSTGRESQL
