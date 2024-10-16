bc95ac98559e9f1dd3817768f7e98b167d8eaa7a
certaays <certaays@gmail.com>
2024-10-16 21:07:27 +0700

    back to origin

--- main.go
+++ main.go
@@ -1,62 +1,24 @@
-// package main
-
-// import (
-// 	"learn-go/database"
-// 	"learn-go/migration"
-// 	"learn-go/route"
-
-// 	"github.com/gofiber/fiber/v2"
-// )
-
-// func Main() {
-// 	//INITIAL DATABASE
-// 	database.DatabaseInit()
-
-// 	//RUN MIGRATION
-// 	migration.RunMigration()
-
-// 	app := fiber.New()
-
-// 	//INITIAL ROUTE
-// 	route.RouteInit(app)
-
-// 	app.Listen(":8080")
-// }
-
-// func main() {
-// 	Main()
-// }
-
-package handler
+package main
 
 import (
 	"learn-go/database"
 	"learn-go/migration"
 	"learn-go/route"
-	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/vercel/go-bridge/go/bridge"
 )
 
-var app *fiber.App
-
-func init() {
-	// INITIAL DATABASE
+func main() {
+	//INITIAL DATABASE
 	database.DatabaseInit()
 
-	// RUN MIGRATION
+	//RUN MIGRATION
 	migration.RunMigration()
 
-	app = fiber.New()
+	app := fiber.New()
 
-	// INITIAL ROUTE
+	//INITIAL ROUTE
 	route.RouteInit(app)
-}
 
-// Handler function to be called by Vercel
-func Handler(ctx *bridge.RequestCtx) {
-	if err := app.Handler()(ctx); err != nil {
-		log.Printf("Error handling request: %v", err)
-	}
+	app.Listen(":8080")
 }
