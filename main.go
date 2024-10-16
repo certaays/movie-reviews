9cdbb16a0f99ccacc87cce7d2aebafcfff49085e
certaays <certaays@gmail.com>
2024-10-16 20:34:25 +0700

    new

--- main.go
+++ main.go
@@ -4,21 +4,20 @@ import (
 	"learn-go/database"
 	"learn-go/migration"
 	"learn-go/route"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/vercel/vercel-go"
 )
 
-func main() {
-	//INITIAL DATABASE
+func handler(w http.ResponseWriter, r *http.Request) {
 	database.DatabaseInit()
-
-	//RUN MIGRATION
 	migration.RunMigration()
-
 	app := fiber.New()
-
-	//INITIAL ROUTE
 	route.RouteInit(app)
+	app.Handler(w, r)
+}
 
-	app.Listen(":8080")
+func main() {
+	vercel.Start(handler)
 }
